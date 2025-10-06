7865876265a25c0daccd1fc0359a5815afebe5c0
Adrian Cervantes Lopez <acervantes-2@cps.edu>
2025-10-06 10:02:15 -0500

    firstTwo

--- a3.py
+++ a3.py
@@ -54,7 +54,12 @@ def title_by_year(matches: List[str]) -> List[str]:
     Returns:
         a list of movie titles made in the passed in year
     """
-    pass
+    year = int(matches[0])
+    result = []
+    for movie in movie_db:
+        if get_year(movie) == year:
+            result.append(get_title(movie))
+    return result
 
 
 def title_by_year_range(matches: List[str]) -> List[str]:
@@ -70,7 +75,13 @@ def title_by_year_range(matches: List[str]) -> List[str]:
         a list of movie titles made during those years, inclusive (meaning if you pass
         in ["1991", "1994"] you will get movies made in 1991, 1992, 1993 & 1994)
     """
-    pass
+    startYear = int(matches[0])
+    endYear = int(matches[1])
+    result = []
+    for movie in movie_db:
+        if startYear <= get_year(movie) <= endYear:
+            result.append(get_title(movie))
+    return result
 
 
 def title_before_year(matches: List[str]) -> List[str]:
